91cc626d2ecfbdf8decee65ea4f4677fa7e826da
jasand86 <joshs@2600hz.com>
2018-01-17 00:56:48 -0800

    home page updates

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){console.log("ayyoo bitch")});
+$(document).ready(function(){$(".project__link").hover(function(){$(this).parents(".showcase-list__project").find(".sidebar").addClass("is-visible")},function(){$(this).parents(".showcase-list__project").find(".sidebar").removeClass("is-visible")})});
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-
-console.log('ayyoo bitch');
+	$('.project__link').hover(
+		function() {
+			$(this).parents('.showcase-list__project').find('.sidebar').addClass('is-visible');
+		}, 
+		function() {
+			$(this).parents('.showcase-list__project').find('.sidebar').removeClass('is-visible');
+		}
+	);
 
 });
