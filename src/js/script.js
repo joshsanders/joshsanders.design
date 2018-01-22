216a797f408c1c28811b11a2a01098a2b19f61d7
jasand86 <joshs@2600hz.com>
2018-01-21 16:14:03 -0800

    all project page skeletons created

--- src/js/script.js
+++ src/js/script.js
@@ -14,8 +14,53 @@ $(document).ready(function() {
 
 	// handles toggle event for the menu
 
-	$('.js-menu-trigger').on('click', function() {
-		$('.menu').toggle('animated pulse');
+	// wrapper function for queueAddClass and chaining
+	$.fn.queueAddClass = function(className) {
+	    this.queue('fx', function(next) {
+	        $(this).addClass(className);
+	        next();
+	    });
+	    return this;
+	};
+
+	// wrapper function for queueRemoveClass to end chaining
+	$.fn.queueRemoveClass = function(className) {
+	    this.queue('fx', function(next) {
+	        $(this).removeClass(className);
+	        next();
+	    });
+	    return this;
+	};
+
+	// $('.js-menu-trigger').on('click', function() {
+	// 	$('.menu').removeClass('bounceOutUp').addClass('bounceInDown').toggleClass('is-visible');
+	// });
+
+	// $('.js-close-menu').on('click', function() {
+	// 	$('.menu').removeClass('bounceInDown').queueAddClass('bounceOutUp').delay(3000).queueRemoveClass('is-visible');
+		
+	// });
+
+	$('.js-menu-trigger').on('click', function(e) {
+		 e.preventDefault();
+
+		$('body').css('overflow', 'hidden');
+		$('.menu-container').removeClass('fadeOut').addClass('fadeIn').toggleClass('is-open');
 	});
+
+	$('.js-close-menu').on('click', function(e) {
+		 e.preventDefault();
+
+		$('body').css('overflow', 'auto');
+		$('.menu-container').removeClass('fadeIn').queueAddClass('fadeOut').delay(1000).queueRemoveClass('is-open');
+		
+	});
+
+
+	
+
+	// $('.js-menu-trigger').on('click', function() {
+	// 	$('.menu').addClass('animated fadeInDown').toggleClass('is-visible');
+	// });
 
 });
