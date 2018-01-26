79cb711859d2f618a7230d46fbf4a65a7ba70cad
jasand86 <joshs@2600hz.com>
2018-01-25 19:48:40 -0800

    homepage responsiveness grid(md) complete

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 
 	// hover class addition for project link in the homepage's 'showcase' section
 
-	$('.project__link').hover(
-		function() {
-			$(this).parents('.showcase-list__project').find('.sidebar').addClass('is-visible');
-		}, 
-		function() {
-			$(this).parents('.showcase-list__project').find('.sidebar').removeClass('is-visible');
-		}
-	);
+	// $('.project__link').hover(
+	// 	function() {
+	// 		$(this).parents('.showcase-list__project').find('.sidebar').addClass('is-visible');
+	// 	}, 
+	// 	function() {
+	// 		$(this).parents('.showcase-list__project').find('.sidebar').removeClass('is-visible');
+	// 	}
+	// );
 
 
 	// handles toggle event for the menu
