58ec2c19ad0e7e7d3f4388a5d3c08da30b3ac246
jasand86 <joshs@2600hz.com>
2018-03-13 20:16:12 -0700

    index intro update

--- dist/index.php
+++ dist/index.php
@@ -143,7 +143,7 @@
 								<path d="M441.9 250.1l-19.8-19.8c-4.7-4.7-12.3-4.7-17 0L250 385.4V44c0-6.6-5.4-12-12-12h-28c-6.6 0-12 5.4-12 12v341.4L42.9 230.3c-4.7-4.7-12.3-4.7-17 0L6.1 250.1c-4.7 4.7-4.7 12.3 0 17l209.4 209.4c4.7 4.7 12.3 4.7 17 0l209.4-209.4c4.7-4.7 4.7-12.3 0-17z"/>
 							</svg>
 
-							<p class="section--intro__content">I'm a multidisciplinary designer based in San Francisco. As the current Lead Product Designer for 2600Hz, I spend my days crafting meaningful products &amp; experiences in the B2B/Enterprise Telecom space.</p>
+							<p class="section--intro__content">Josh is a multidisciplinary designer in San Francisco. Currently Lead Product Designer at 2600Hz<span class="dash-spacer"></span>&mdash;<span class="dash-spacer"></span>crafting meaningful products &amp; experiences in the B2B/Enterprise Telecom space.</p>
 						</div>
 					</div>
 				</div>
